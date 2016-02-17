0535c62e1ce0aad9dfc1dc2e83014ff487ea8abc
talmeym <mtalmey@gmail.com>
2016-02-17 14:48:44 +0000

    added fullLoad test for missing xml loaders

--- src/main/java/com/emarte/regurgitator/extensions/ExtensionsEntityPack.java
+++ src/main/java/com/emarte/regurgitator/extensions/ExtensionsEntityPack.java
@@ -2,8 +2,11 @@ package com.emarte.regurgitator.extensions;
 
 import com.emarte.regurgitator.core.AbstractEntityPack;
 
+import java.util.HashMap;
+
 public class ExtensionsEntityPack extends AbstractEntityPack {
 	public ExtensionsEntityPack() {
 		addConditionBehaviour("contains-json-path", new ContainsJsonPath());
+		addConditionBehaviour("contains-xpath", new ContainsXpath(new HashMap<String, String>()));
 	}
 }
