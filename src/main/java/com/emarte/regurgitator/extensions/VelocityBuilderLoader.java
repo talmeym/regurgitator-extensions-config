6b77c7c15d94247689d5fdfa27d8ce179b51a61f
talmeym <mtalmey@gmail.com>
2017-10-07 15:08:44 +0100

    Applied MIT License source comments

--- src/main/java/com/emarte/regurgitator/extensions/VelocityBuilderLoader.java
+++ src/main/java/com/emarte/regurgitator/extensions/VelocityBuilderLoader.java
@@ -1,28 +1,34 @@
+/*
+ * Copyright (C) 2017 Miles Talmey.
+ * Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
+ */
 package com.emarte.regurgitator.extensions;
 
 import com.emarte.regurgitator.core.*;
 
-public abstract class VelocityBuilderLoader {
-	protected ValueBuilder buildVelocityValueBuilder(String source, String value, String file, boolean allContexts, Log log) throws RegurgitatorException {
-		int numberSet = 0;
-		numberSet = source != null ? ++numberSet : numberSet;
-		numberSet = value != null ? ++numberSet : numberSet;
-		numberSet = file != null ? ++numberSet : numberSet;
+import static com.emarte.regurgitator.core.FileUtil.streamToString;
 
-		if(numberSet != 1) {
-			throw new RegurgitatorException("One of source, value or file is required");
-		}
+abstract class VelocityBuilderLoader {
+    ValueBuilder buildVelocityValueBuilder(String source, String value, String file, boolean allContexts, Log log) throws RegurgitatorException {
+        int numberSet = 0;
+        numberSet = source != null ? ++numberSet : numberSet;
+        numberSet = value != null ? ++numberSet : numberSet;
+        numberSet = file != null ? ++numberSet : numberSet;
 
-		if(file != null) {
-			try {
-				value = FileUtil.streamToString(FileUtil.getInputStreamForFile(file));
-			} catch (Exception e) {
-				throw new RegurgitatorException("Error loading file: " + file, e);
-			}
-		}
+        if(numberSet != 1) {
+            throw new RegurgitatorException("One of source, value or file is required");
+        }
 
-		ContextLocation location = source != null ? new ContextLocation(source) : null;
-		log.debug("Loaded velocity builder");
-		return new VelocityBuilder(new ValueSource(location, value), allContexts);
-	}
+        if(file != null) {
+            try {
+                value = streamToString(FileUtil.getInputStreamForFile(file));
+            } catch (Exception e) {
+                throw new RegurgitatorException("Error loading file: " + file, e);
+            }
+        }
+
+        ContextLocation location = source != null ? new ContextLocation(source) : null;
+        log.debug("Loaded velocity builder");
+        return new VelocityBuilder(new ValueSource(location, value), allContexts);
+    }
 }
