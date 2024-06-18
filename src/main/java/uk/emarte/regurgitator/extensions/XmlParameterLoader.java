1c48375fa01b5cf94af6332b8d6ebf509322aa8c
talmeym <mtalmey@gmail.com>
2024-06-18 11:42:48 +0100

    optional

--- src/main/java/uk/emarte/regurgitator/extensions/XmlParameterLoader.java
+++ src/main/java/uk/emarte/regurgitator/extensions/XmlParameterLoader.java
@@ -11,8 +11,8 @@ import java.util.List;
 import static uk.emarte.regurgitator.core.ValueSourceLoader.loadValueSource;
 
 abstract class XmlParameterLoader {
-    Step buildXmlParameter(String id, ParameterPrototype prototype, String context, String source, String value, String file, List<ValueProcessor> processors, XpathProcessor xpathProcessor, Log log) throws RegurgitatorException {
+    Step buildXmlParameter(String id, ParameterPrototype prototype, String context, String source, String value, String file, List<ValueProcessor> processors, XpathProcessor xpathProcessor, boolean optional, Log log) throws RegurgitatorException {
         log.debug("Loaded xml parameter '{}'", id);
-        return new XmlParameter(id, prototype, context, loadValueSource(source, value, file), xpathProcessor, processors);
+        return new XmlParameter(id, prototype, context, loadValueSource(source, value, file), xpathProcessor, processors, optional);
     }
 }
